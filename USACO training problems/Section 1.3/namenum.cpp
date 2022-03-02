c8107a013e2bc19c531fae81018b78a8fc76a89f
Dilet the Lemon <50088723+DILET1@users.noreply.github.com>
2022-03-02 10:29:40 -0800

    veri ebic solution
    ebic

--- USACO training problems/Section 1.3/namenum.cpp
+++ USACO training problems/Section 1.3/namenum.cpp
@@ -1,24 +1,46 @@
 /*
-ID:ethanhp1
 TASK:namenum
+ID:ethanhp1
 LANG:C++14
 */
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <algorithm>
+#include <vector>
 using namespace std;
+string get_id(string name){
+	char decoder[24] = {'2','2','2','3','3','3','4','4','4','5','5','5','6','6','6','7','7','7','8','8','8','9','9','9',};
+	string id;
+	for(int i = 0; i < name.length(); i++){
+		int index = name[i]-'A';
+		if(name[i] > 'P'){
+			index--;
+		}
+		id.push_back(decoder[index]);
+	}
+	return id;
+}
 int main(){
-	ifstream dictr("dict.txt");
+	ifstream dict("dict.txt");
 	ifstream is("namenum.in");
 	ofstream of("namenum.out");
-	string dict[4617];
-	string name;
-	for(int i = 0; i < 4617; i++){
-		dictr >> name;
-		dict[i] = name;
+	
+	string num;
+	is >> num;
+	string curname;
+	bool hasName = false;
+	while(dict >> curname){
+		curname.erase(remove(curname.begin(), curname.end(), '\n'), curname.end());
+		if(get_id(curname) == num){
+			hasName = true;
+			of << curname<<endl;
+		}
 	}
-	cout<<dict[4616];
-	string serial;
-	is >> serial;
+	if(!hasName){
+		of << "NONE"<<endl;
+	}
+	of.close();
+	return 0;
 }
