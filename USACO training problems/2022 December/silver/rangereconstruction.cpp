93201e1655a35b350e620a9816a47cb180f82e0c
Dilet the Lemon <50088723+DILET1@users.noreply.github.com>
2022-12-21 16:27:28 -0800

    Add files via upload

---
+++ USACO training problems/2022 December/silver/rangereconstruction.cpp
@@ -0,0 +1,110 @@
+//we like the pain (pain) - average cyberpunk music
+//me rn: yes i can feel and touch it (the pain)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	
+	int n;
+	cin >> n;
+	vector<vector<int>> lists;
+	//takes input. Do note that the subarray sizes are shrinking from N to 1.
+	for(int i = n; i > 0; i--){
+		
+		vector<int> list;
+		for(int j = 0; j < i; j++){
+		
+			int temp;
+			cin >> temp;
+			list.push_back(temp);
+		
+		}
+		
+		lists.push_back(list);
+	}
+	
+//	for(int i = n; i > 0; i--){
+//		
+//		for(int j = 0; j < i; j++){
+//		
+//			cout<<lists[n-i][j];
+//		
+//		}
+//		cout<<endl;
+//	
+//	}
+	
+	vector<int> deltas;
+	//initial stuff. We set the start as 0, deltas stores the offset from start.
+	deltas.push_back(0);
+	//the second value can be either + or - the delta, it's arbitrary
+	deltas.push_back(lists[0][1]);
+	
+	for(int c = 2; c < n; c++){
+		
+		cout<<"PASS"<<endl;
+		//2 arrays with each of the possible new offsets added to it.
+		int p1 = deltas[c-1] + lists[c-1][1];
+		int p2 = deltas[c-1] - lists[c-1][1];
+		vector<int> withP1 = deltas;
+		withP1.push_back(p1);
+		vector<int> withP2 = deltas;
+		withP2.push_back(p2);
+		cout<<p1<<" "<<p2<<endl;
+		
+		//if the offset is 0
+		if(p1 == p2){
+			deltas.push_back(p1);
+		}
+		
+		else{
+			
+			bool isP1 = false;
+			for(int b = c-1; b >=0; b--){
+				//ALL THE ITERATORS!
+				auto s1 = withP1.begin() + b;
+				auto s2 = withP2.begin() + b;
+				auto e1 = withP1.begin() + c+1;
+				auto e2 = withP2.begin() + c+1;
+				//checks to see if the ranges of the subarrays match. Until it either reaches the start of the array or finds one that doesn't, start checking the total range of the array for the bth element of deltas. (conveniently stored as the last value there.)
+				if(*max_element(s1,e1) - *min_element(s1,e1)  == lists[b][n-b-1] && *max_element(s2,e2) - *min_element(s2,e2)  == lists[b][n-b-1]){
+				
+					continue;
+				
+				}
+				else if(*max_element(s1,e1) - *min_element(s1,e1)  == lists[b][n-b-1] && *max_element(s2,e2) -*min_element(s2,e2) != lists[b][n-b-1]){
+				
+					cout<<"P1 TRUE"<<endl;
+					isP1 = true;
+					break;
+				}
+				
+				else if(*max_element(s1,e1) - *min_element(s1,e1)  != lists[b][n-b-1] && *max_element(s2,e2) -*min_element(s2,e2) == lists[b][n-b-1]){
+				
+					cout<<"P2 TRUE"<<endl;
+					break;
+				
+				}
+			}
+			//:)
+			if(isP1){
+			
+				deltas.push_back(p1);
+			
+			}
+			
+			else if(!isP1){
+				
+				deltas.push_back(p2);
+				
+			}
+			
+		}
+	}
+	
+	for(int p = 0; p < n; p++){
+		
+		cout<<deltas[p]<<" ";
+		
+	}
+	cout<<'\n';
+}
