a967a13cbe9596c4daff726e819e74a268174815
Dilet the Lemon <50088723+DILET1@users.noreply.github.com>
2023-12-10 11:57:08 -0800

    PAIN
    finished cowchecklist after a super long time

--- Alphastar/CC41B/Dynamic Programming on a Multidimension Array/cowchecklist.cpp
+++ Alphastar/CC41B/Dynamic Programming on a Multidimension Array/cowchecklist.cpp
@@ -13,19 +13,25 @@ int main(){
 		cin >> a >> b;
 		ph.push_back({a, b});
 	}
-	for(int i = 0; i < h; i++){
+	for(int i = 0; i < g; i++){
 		int a, b;
 		cin >> a >> b;
 		pg.push_back({a, b});
 	}
-	vector<vector<int>> dp(h+1, vector<int>(g+1, 2147483647));
-	dp[1][0] = 0;
-	for(int i =1; i < h-2; i++){
-		for(int j = 0; j < g-1; j++){
-			//choice 1: to next H
-			dp[i+1][j] = min(dp[i+1][j], dp[i][j] + dist)
-			//choice 2: to next G
-			
+	//my brain hurts from abstracting all of this
+	vector<vector<vector<int>>> dp(h+2, vector<vector<int>>(g+2, vector<int>(2, 2000000000))); //dp[i][j][0] is the shortest way to finish visiting i holsteins and j gurnseys, ending on a holstein. dp[i][j][1] ends on a gurnsey.
+	dp[1][0][0] = 0;
+	for(int i = 1; i <= h; i++){
+		for(int j = 0; j <= g; j++){
+			dp[i+1][j][0] = min(dp[i+1][j][0], dp[i][j][0] + getDist(ph[i-1],ph[i]));
+			dp[i][j+1][1] = min(dp[i][j+1][1], dp[i][j][0] + getDist(ph[i-1],pg[j]));
+			if(j != 0){
+				dp[i+1][j][0] = min(dp[i+1][j][0], dp[i][j][1] + getDist(ph[i],pg[j-1]));
+				dp[i][j+1][1] = min(dp[i][j+1][1], dp[i][j][1] + getDist(pg[j-1],pg[j]));
+			}
+//			cout<<i+1<<" "<<j<<" "<<dp[i+1][j][0]<<'\n';
+//			cout<<i<<" "<<j+1<<" "<<dp[i][j+1][1]<<'\n';
 		}
 	}
+	cout<<dp[h][g][0]<<'\n';
 }
